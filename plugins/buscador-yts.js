e44356d327fdcc09c9d80303cf2a83a95f12abe4
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2022-09-03 18:26:13 -0300

    Add files via upload

---
+++ plugins/buscador-yts.js
@@ -0,0 +1,27 @@
+import { youtubeSearch } from '@bochilteam/scraper'
+let handler = async (m, { text }) => {
+if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙾 𝙲𝙰𝙽𝙰𝙻 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*'
+const { video, channel } = await youtubeSearch(text)
+let teks = [...video, ...channel].map(v => {
+switch (v.type) {
+case 'video': return `
+📌 *${v.title}* (${v.url})
+⌚ 𝙳𝚞𝚛𝚊𝚌𝚒𝚘𝚗: ${v.durationH}
+⏲️ 𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚍𝚘 ${v.publishedTime}
+👁️ ${v.view} 𝚟𝚒𝚜𝚝𝚊𝚜
+`.trim()
+case 'channel': return `
+📌 *${v.channelName}* (${v.url})
+🧑‍🤝‍🧑 _${v.subscriberH} 𝚜𝚞𝚜𝚌𝚛𝚒𝚙𝚝𝚘𝚛𝚎𝚜_
+🎥 ${v.videoCount} 𝚟𝚒𝚍𝚎𝚘𝚜
+`.trim()
+}
+}).filter(v => v).join('\n\n========================\n\n')
+m.reply(teks)
+}
+handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
+handler.tags = ['tools']
+handler.command = /^yts(earch)?$/i
+handler.register = true
+handler.limit = 1
+export default handler
